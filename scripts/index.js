daea1bafa577b05bb506f3e7563f0bf564590ca6
gustavoalvim41 <gustavoalvim85@gmail.com>
2025-01-15 15:55:30 -0300

    update script

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,5 @@
 const sectionhero = document.querySelector('.hero')
-const sectionCommands = document.querySelector('.commands')
-const loading = document.querySelector('.loading')
 const footerInformation = document.querySelector('.information')
-const footerCredits = document.querySelector('.credits')
 
 const btnDiscord = document.querySelector('.btn-discord')
 btnDiscord.addEventListener("click", function () {
@@ -13,16 +10,3 @@ const btnCommands = document.querySelector('.btn-secondary')
 btnCommands.addEventListener('click', function () {
 	loadingCommands()
 })
-
-const loadingCommands = () => {
-	loading.classList.remove('invisible')
-	sectionhero.classList.add('invisible')
-	footerInformation.classList.add('invisible')
-    setTimeout(showCommands, 1000 * 1.5)
-}
-
-const showCommands = () => {
-	sectionCommands.classList.remove('invisible')
-	footerCredits.classList.remove('invisible')
-	loading.classList.add('invisible')
-}
